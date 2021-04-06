36c9064836f7ed9fa42f7b28494f72c04c0a6fe5
Abhisek Roy <abhisek.kai@gmail.com>
2021-04-06 12:48:41 +0530

    Intro to override and Super

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,6 +2,16 @@
 
 #include "TankPlayerController.h"
 
+
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    ATank* PawnTank = GetControlledTank();
+    if(!PawnTank) return;
+    UE_LOG(LogTemp, Warning, TEXT("TankPlayerController for: %s"), *(PawnTank->GetName()));
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
