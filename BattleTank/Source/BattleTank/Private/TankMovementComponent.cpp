c60a8c4e47bf02d71b6331d91cd9f2c56773a3d2
Abhisek Roy <abhisek.kai@gmail.com>
2021-04-15 20:49:14 +0530

    Introduced sideways friction for earlier stop. The application points, bias can be tuned.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -3,6 +3,52 @@
 
 #include "TankMovementComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "DrawDebugHelpers.h"
+
+UTankMovementComponent::UTankMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	if (TankRoot->GetComponentVelocity().Size() < 100.0)  // Magic number to ignore sideways friction a lower speed
+	{
+		return;
+	}
+	
+	// Apply extra force to counter sideways slip
+	auto Velocity = TankRoot->GetComponentVelocity();
+	auto SlippageSpeed = FVector::DotProduct(Velocity.GetSafeNormal(), TankRoot->GetRightVector());
+	auto SlippageForce = -MaxTractiveForce * SlippageSpeed * TankRoot->GetRightVector(); // Two application points
+
+	FVector ApplicationPointF = TankRoot->GetComponentLocation() + TankRoot->GetUpVector()*CGHeight + TankRoot->GetForwardVector()*CGOffset;
+	FVector ApplicationPointR = TankRoot->GetComponentLocation() + TankRoot->GetUpVector()*CGHeight - TankRoot->GetForwardVector()*CGOffset;
+
+	TankRoot->AddForceAtLocation(SlippageForce*(1 - Bias), ApplicationPointF);
+	TankRoot->AddForceAtLocation(SlippageForce*(Bias), ApplicationPointR);
+	DrawDebugLine(
+		GetWorld(),
+		ApplicationPointF,
+		ApplicationPointF + SlippageForce.GetSafeNormal()*500,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+	DrawDebugLine(
+		GetWorld(),
+		ApplicationPointR,
+		ApplicationPointR + SlippageForce.GetSafeNormal()*500,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+}
 
 void UTankMovementComponent::Initialize(UStaticMeshComponent* LeftTrackToSet, UStaticMeshComponent* RightTrackToSet)
 {
@@ -31,9 +77,9 @@ void UTankMovementComponent::ApplyThrottleIndividually(float ThrottleLeft, float
 
 	if (!ensure(LeftTrack && RightTrack)) return;
 
-	auto LeftForce = LeftTrack->GetForwardVector() * LeftForceMag;
-	auto RightForce = RightTrack->GetForwardVector() * RightForceMag;
-
+	LeftForce = LeftTrack->GetForwardVector() * LeftForceMag;
+	RightForce = RightTrack->GetForwardVector() * RightForceMag;
+	
 	LeftTrack->AddForceAtLocation(LeftForce, LeftTrack->GetComponentLocation());
     RightTrack->AddForceAtLocation(RightForce, RightTrack->GetComponentLocation());
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -38,5 +38,20 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, Category = Movement)
 	float MaxTractiveForce = 30000000.0f;
+	
+	UTankMovementComponent();
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+private:
+	FVector LeftForce = FVector(0);
+	FVector RightForce = FVector(0);
+
+	// Rear Bias, Front Bias = 1 - Bias value
+	UPROPERTY(EditDefaultsOnly, Category = "Tuning", meta = (ClampMin = "0.0", ClampMax = "0.99", UIMin = "0.0", UIMax = "0.99"))
+	float Bias = 0.8f;
+	UPROPERTY(EditDefaultsOnly, Category = "Tuning", meta = (ClampMin = "0.0", ClampMax = "300", UIMin = "0.0", UIMax = "300"))
+	float CGHeight = 100.f;
+	UPROPERTY(EditDefaultsOnly, Category = "Tuning", meta = (ClampMin = "0.0", ClampMax = "300", UIMin = "0.0", UIMax = "300"))
+	float CGOffset = 200.f;
 };
 
